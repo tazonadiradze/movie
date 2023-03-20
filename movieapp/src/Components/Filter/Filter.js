276a74697392228e7afb40a36fd6caba5bdcf56e
Tazo <tamaznadiradze2002@gmail.com>
2023-03-20 12:48:02 +0400

    feat: move filter to another component

---
+++ movieapp/src/Components/Filter/Filter.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import Data from '../../../src/Data.json'
+import Movies from '../Movies/Movies'
+import './Filter.css'
+const Filter = () => {
+
+ const [search, setSearch] = useState('')
+ console.log(Data)
+ const filteredData = Data.filter((item) => {
+  return item.Title.toLowerCase().includes(search.toLowerCase())
+ })
+ return (
+  <div className='allWraper'>
+   <div className='search'>
+    <input value={search} type='text' className='input' onChange={(e) => { setSearch(e.target.value) }} placeholder='Search' />
+   </div>
+   <Movies filter={filteredData} />
+  </div>
+ )
+
+
+
+}
+
+export default Filter
